74f8907bbae97b86b9393b8b84a98cbacca215bf
GoldenCrater <git@gc.gg>
2022-06-08 11:07:53 +0100

    Changes for EN and JP updates

--- src/composables/RegionSettings.js
+++ src/composables/RegionSettings.js
@@ -34,9 +34,9 @@ const jpRegionSettings = {
 };
 
 const globalRegionSettings = {
-    MaxLevel: 73,
-    UniqueWeaponsAvailable: false,
-    UniqueWeaponMaxStar: 0,
-    MaxEquipLevel: 5,
-    MaxBond: 20,
+    MaxLevel: 75,
+    UniqueWeaponsAvailable: true,
+    UniqueWeaponMaxStar: 3,
+    MaxEquipLevel: 6,
+    MaxBond: 50,
 };
